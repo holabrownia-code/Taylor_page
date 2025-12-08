57c70ca4e49f2b9afb97a23a28614774be5989a7
Matias <matias@suntzu-growth.com>
2025-12-08 19:14:52 +0100

    Fix invalid JSX in temp-images page (retry)

--- app/temp-images/page.tsx
+++ app/temp-images/page.tsx
@@ -1,23 +1,23 @@
-
-<html>
-    <body style="background: #333; color: white;">
-        <h1>Image Identification</h1>
-        <div>
-            <h2>temp_0.png</h2>
-            <img src="/images/temp_0.png" style="max-width: 300px; border: 1px solid white;">
+export default function TempImagesPage() {
+    return (
+        <div style={{ background: "#333", color: "white", minHeight: "100vh", padding: "20px" }}>
+            <h1>Image Identification</h1>
+            <div>
+                <h2>temp_0.png</h2>
+                <img src="/images/temp_0.png" style={{ maxWidth: "300px", border: "1px solid white" }} alt="temp_0" />
+            </div>
+            <div>
+                <h2>temp_1.jpg</h2>
+                <img src="/images/temp_1.jpg" style={{ maxWidth: "300px", border: "1px solid white" }} alt="temp_1" />
+            </div>
+            <div>
+                <h2>temp_2.png</h2>
+                <img src="/images/temp_2.png" style={{ maxWidth: "300px", border: "1px solid white" }} alt="temp_2" />
+            </div>
+            <div>
+                <h2>temp_3.jpg</h2>
+                <img src="/images/temp_3.jpg" style={{ maxWidth: "300px", border: "1px solid white" }} alt="temp_3" />
+            </div>
         </div>
-        <div>
-            <h2>temp_1.jpg</h2>
-            <img src="/images/temp_1.jpg" style="max-width: 300px; border: 1px solid white;">
-        </div>
-        <div>
-            <h2>temp_2.png</h2>
-            <img src="/images/temp_2.png" style="max-width: 300px; border: 1px solid white;">
-        </div>
-        <div>
-            <h2>temp_3.jpg</h2>
-            <img src="/images/temp_3.jpg" style="max-width: 300px; border: 1px solid white;">
-        </div>
-    </body>
-</html>
-
+    )
+}
